61943bb3b2f98056de59bf3e90dd59bc8a77ad4b
Floris Vleugels <florisvleugels@hotmail.com>
2024-07-23 23:00:44 +0200

    refactor

--- src/scenes/Floor.cpp
+++ src/scenes/Floor.cpp
@@ -1,71 +1,42 @@
 #include "Floor.h"
 
 Floor::Floor() {
-    layer0Texture.loadFromFile("../resources/para/Layer_0000_9.png");
-    layer0Sprite.setTexture(layer0Texture);
-    layer0Sprite.setPosition(0, 0);
 
-    layer1Texture.loadFromFile("../resources/para/Layer_0001_8.png");
-    layer1Sprite.setTexture(layer1Texture);
-    layer1Sprite.setPosition(0, 0);
+    std::vector<std::string> textureFiles = {
+        "../resources/para/Layer_0011_0.png",
+        "../resources/para/Layer_0010_1.png",
+        "../resources/para/Layer_0009_2.png",
+        "../resources/para/Layer_0008_3.png",
+        "../resources/para/Layer_0007_Lights.png",
+        "../resources/para/Layer_0006_4.png",
+        "../resources/para/Layer_0005_5.png",
+        "../resources/para/Layer_0004_Lights.png",
+        "../resources/para/Layer_0003_6.png",
+        "../resources/para/Layer_0002_7.png",
+        "../resources/para/Layer_0001_8.png",
+        "../resources/para/Layer_0000_9.png",
+    };
 
-    layer2Texture.loadFromFile("../resources/para/Layer_0002_7.png");
-    layer2Sprite.setTexture(layer2Texture);
-    layer2Sprite.setPosition(0, 0);
+    textures.resize(textureFiles.size());
+    sprites.resize(textureFiles.size());
 
-    layer3Texture.loadFromFile("../resources/para/Layer_0003_6.png");
-    layer3Sprite.setTexture(layer3Texture);
-    layer3Sprite.setPosition(0, 0);
-
-    layer4Texture.loadFromFile("../resources/para/Layer_0004_Lights.png");
-    layer4Sprite.setTexture(layer4Texture);
-    layer4Sprite.setPosition(0, 0);
-
-    layer5Texture.loadFromFile("../resources/para/Layer_0005_5.png");
-    layer5Sprite.setTexture(layer5Texture);
-    layer5Sprite.setPosition(0, 0);
-
-    layer6Texture.loadFromFile("../resources/para/Layer_0006_4.png");
-    layer6Sprite.setTexture(layer6Texture);
-    layer6Sprite.setPosition(0, 0);
-
-    layer7Texture.loadFromFile("../resources/para/Layer_0007_Lights.png");
-    layer7Sprite.setTexture(layer7Texture);
-    layer7Sprite.setPosition(0, 0);
-
-    layer8Texture.loadFromFile("../resources/para/Layer_0008_3.png");
-    layer8Sprite.setTexture(layer8Texture);
-    layer8Sprite.setPosition(0, 0);
-
-    layer9Texture.loadFromFile("../resources/para/Layer_0009_2.png");
-    layer9Sprite.setTexture(layer9Texture);
-    layer9Sprite.setPosition(0, 0);
-
-    layer10Texture.loadFromFile("../resources/para/Layer_0010_1.png");
-    layer10Sprite.setTexture(layer10Texture);
-    layer10Sprite.setPosition(0, 0);
-
-    layer11Texture.loadFromFile("../resources/para/Layer_0011_0.png");
-    layer11Sprite.setTexture(layer11Texture);
-    layer11Sprite.setPosition(0, 0);
-
-    testrect.setSize(sf::Vector2f(50, 800));
-    testrect.setPosition(1000,0);
+    for (size_t i = 0; i < textureFiles.size(); ++i) {
+        if (!textures[i].loadFromFile(textureFiles[i])) {
+            // Handle loading error
+        }
+        sprites[i].setTexture(textures[i]);
+    }
 }
 
 void Floor::draw(sf::RenderWindow& window) {
-    window.draw(layer11Sprite);
-    window.draw(layer10Sprite);
-    window.draw(layer9Sprite);
-    window.draw(layer8Sprite);
-    window.draw(layer7Sprite);
-    window.draw(layer6Sprite);
-    window.draw(layer5Sprite);
-    window.draw(layer4Sprite);
-    window.draw(layer3Sprite);
-    window.draw(layer2Sprite);
-    window.draw(layer1Sprite);
-    window.draw(layer0Sprite);
-    //window.draw(testrect);
+
+    int numTiles = window.getSize().x / textureWidth + 1;
+
+    for (int i = 0; i < numTiles; ++i) {
+        for (auto& sprite : sprites) {
+            sprite.setPosition(i * textureWidth, 0);
+            window.draw(sprite);
+        }
+    }
 }
 
--- src/scenes/Floor.h
+++ src/scenes/Floor.h
@@ -2,7 +2,7 @@
 #define FLOOR_H
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
+#include <vector>
 
 class Floor {
 public:
@@ -10,44 +10,11 @@ public:
     void draw(sf::RenderWindow& window);
 
 private:
-    sf::Sprite layer0Sprite;
-    sf::Texture layer0Texture;
+    std::vector<sf::Texture> textures;
+    std::vector<sf::Sprite> sprites;
+    const float textureWidth = 928.0f; //float textureWidth = layer0Texture.getSize().x;
 
-    sf::Sprite layer1Sprite;
-    sf::Texture layer1Texture;
-
-    sf::Sprite layer2Sprite;
-    sf::Texture layer2Texture;
-
-    sf::Sprite layer3Sprite;
-    sf::Texture layer3Texture;
-
-    sf::Sprite layer4Sprite;
-    sf::Texture layer4Texture;
-
-    sf::Sprite layer5Sprite;
-    sf::Texture layer5Texture;
-
-    sf::Sprite layer6Sprite;
-    sf::Texture layer6Texture;
-
-    sf::Sprite layer7Sprite;
-    sf::Texture layer7Texture;
-
-    sf::Sprite layer8Sprite;
-    sf::Texture layer8Texture;
-
-    sf::Sprite layer9Sprite;
-    sf::Texture layer9Texture;
-
-    sf::Sprite layer10Sprite;
-    sf::Texture layer10Texture;
-
-    sf::Sprite layer11Sprite;
-    sf::Texture layer11Texture;
-
-    sf::RectangleShape testrect;
 };
 
-#endif
+#endif // FLOOR_H
 
